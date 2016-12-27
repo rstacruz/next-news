56f9595b5d4bb0fc433211a726e27eab59270d63
Guillermo Rauch <rauchg@gmail.com>
2016-12-26 16:29:05 -0800

    login-form: fix paragraphs

--- components/login-form.js
+++ components/login-form.js
@@ -1,12 +1,12 @@
 export default () => (
   <div>
     <h4>Login</h4>
-    <p className={p}>username: <input /><br/>
+    <p>username: <input /><br/>
     password: <input /></p>
     <button>login</button>
     <p><a href="#">Forgot your password?</a></p>
     <h4>Create Account</h4>
-    <p className={p}>username: <input /><br/>
+    <p>username: <input /><br/>
     password: <input /></p>
     <button>create account</button>
 
