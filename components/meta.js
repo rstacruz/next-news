93861166d8380b174e06effd079fa768c3e694cd
Guillermo Rauch <rauchg@gmail.com>
2016-12-31 11:07:21 -0800

    nprogress

--- components/meta.js
+++ components/meta.js
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = () => NProgress.start()
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 export default () => (
   <div>
@@ -16,6 +22,32 @@ export default () => (
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+      }
+
+      /* loading progress bar styles */
+      #nprogress {
+        pointer-events: none;
+      }
+
+      #nprogress .bar {
+        background: #ff9300;
+        position: fixed;
+        z-index: 1031;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 2px;
+      }
+
+      #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px #ff9300, 0 0 5px #ff9300;
+        opacity: 1.0;
+        transform: rotate(3deg) translate(0px, -4px);
       }
     `}</style>
   </div>
